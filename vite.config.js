87b26512737c0ee48e51ffecb09d51197f37d58a
floryid <nofriintraco@gmail.com>
2025-04-04 23:47:39 +0900

    perubahan

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/kurs/',
   server: {
     port: 3000,
     open: true
